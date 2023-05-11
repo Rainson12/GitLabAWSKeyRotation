d82e7490ef2b0e3983b43c71483468d328710e35
Rene Steen <steen.rene@indivumed.com>
2023-05-11 18:14:47 +0200

    updated error message when invalid password was provided

--- Backend/GitLabAWSKeyRotation.Domain/Common/Errors/Authentication.cs
+++ Backend/GitLabAWSKeyRotation.Domain/Common/Errors/Authentication.cs
@@ -6,6 +6,6 @@ namespace GitLabAWSKeyRotation.Domain.Common.Errors
     {
         public static Error NotConfigured => Error.Conflict("Authentication.NotConfigured", "The authentication is not configured.");
         public static Error AlreadyConfigured => Error.Conflict("Authentication.AlreadyConfigured", "The authentication is already configured.");
-        public static Error IncorrectAuthKey => Error.Validation("Authentication.IncorrectAuthKey", "The authentication failed.");
+        public static Error IncorrectAuthKey => Error.Validation("Authentication.IncorrectAuthKey", "Incorrect password provided.");
     }
 }
